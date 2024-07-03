774252e0604b9a3fd271409a9a3ed017ef533f06
Sergio Rua <sergio.rua@digitalis.io>
2024-07-03 08:37:04 +0100

    Python 3.11 support

--- v6.1.0-ACv4.1.0/cqlshlib/saferscanner.py
+++ v6.1.0-ACv4.1.0/cqlshlib/saferscanner.py
@@ -19,7 +19,10 @@
 # regex in-pattern flags. Any of those can break correct operation of Scanner.
 
 import re
-from sre_constants import BRANCH, SUBPATTERN, GROUPREF, GROUPREF_IGNORE, GROUPREF_EXISTS
+try:
+    from sre_constants import BRANCH, SUBPATTERN, GROUPREF, GROUPREF_IGNORE, GROUPREF_EXISTS
+except ImportError:
+    from re._constants import BRANCH, SUBPATTERN, GROUPREF, GROUPREF_IGNORE, GROUPREF_EXISTS
 from sys import version_info
 
 
@@ -80,5 +83,25 @@ class Py38SaferScanner(SaferScannerBase):
         self.p = p
         self.scanner = re.sre_compile.compile(p)
 
+class Py311SaferScanner(SaferScannerBase):
 
-SaferScanner = Py38SaferScanner if version_info >= (3, 8) else Py36SaferScanner
+    def __init__(self, lexicon, flags=0):
+        self.lexicon = lexicon
+        p = []
+        s = re._parser.State()
+        s.flags = flags
+        for phrase, action in lexicon:
+            gid = s.opengroup()
+            p.append(re._parser.SubPattern(s, [(SUBPATTERN, (gid, 0, 0, re._parser.parse(phrase, flags))), ]))
+            s.closegroup(gid, p[-1])
+        p = re._parser.SubPattern(s, [(BRANCH, (None, p))])
+        self.p = p
+        self.scanner = re._compiler.compile(p)
+
+
+if version_info >= (3, 11):
+    SaferScanner = Py311SaferScanner
+elif version_info >= (3, 8):
+    SaferScanner = Py38SaferScanner
+else:
+    SaferScanner = Py36SaferScanner
